fe724eaab44444817ee7c72bffcaa6fb44d8c4ec
Victor <Victor@Victors-MacBook-Pro.local>
2019-01-03 23:26:18 +0000

    added the list functionality and experimented with debugger

--- notes.js
+++ notes.js
@@ -1,5 +1,3 @@
-console.log('starting notes.js');
-
 const fs = require('fs');
 
 var fetchNotes = () => {
@@ -33,7 +31,7 @@ var addNote = (title, body) => {
 };
 
 var getAll = () => {
-    console.log('getting all notes');
+    return fetchNotes();
 };
 
 var getNote = (title) => {
@@ -50,6 +48,7 @@ var removeNote = (title) => {
 };
 
 var logNote = (note) => {
+    debugger;
     console.log('--');
     console.log('Title: ' + note.title);
     console.log('Body: ' + note.body);
---
+++ playground/debugging.js
@@ -0,0 +1,9 @@
+var person = {
+    name: 'vic'
+};
+
+person.age = 25;
+
+person.name = 'like';
+
+console.log(person);
